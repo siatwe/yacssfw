22da766dccd5454e115e847ef9dd439ab1265a38
siatwe <siatwe@tutanota.com>
2017-09-21 16:18:25 +0200

    update: update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+cp src/less/main.css dist/yacssfw.css
+cp src/js/main.js dist/yacssfw.js
 yuicompressor -o dist/yacssfw.min.css src/less/main.css
 yuicompressor -o dist/yacssfw.min.js src/js/main.js
 git add .
