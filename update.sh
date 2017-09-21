fb0fab15f5fc6fcd0a20542143736f325b45e072
siatwe <siatwe@tutanota.com>
2017-09-21 16:04:12 +0200

    changed: update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-yuicompressor -o dist/yacssfw.min.css src/less/main.css && yuicompressor -o dist/yacssfw.min.js src/js/main.js
+yuicompressor -o dist/yacssfw.min.css src/less/main.css
+yuicompressor -o dist/yacssfw.min.js src/js/main.js
 git add .
 git commit
-git pull
+git push
